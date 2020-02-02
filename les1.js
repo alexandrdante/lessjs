4779d2345bad47cde92cbb21a51c7bdb3cd845aa
Alexandr <alexandrdante@gmail.com>
2020-02-03 00:14:30 +0300

    Add first comm

---
+++ les1.js
@@ -0,0 +1,22 @@
+let persone = {
+    name: "John",
+    age: "25",
+    isMarried: false
+};
+
+console.log(persone["name"]);
+
+let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+
+console.log(arr["0"]);
+
+//alert("Hello, world");
+
+//let answer = confirm("Are you here?");
+
+//console.log(answer);
+
+
+let answer = prompt("Есть ли вам 18?", "Да");
+
+console.log(typeof(arr));
